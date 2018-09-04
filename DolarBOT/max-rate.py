0f74d9ef56ba2cb526999aecfd3e62e348206fd1
Eze Brotsky <eze_brotsky@hotmail.com>
2018-09-03 21:50:19 -0300

    Prueba max-rate.py

---
+++ DolarBOT/max-rate.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+from datetime import datetime, timedelta
+
+import tweepy, urllib, json, mysql.connector
+
+# == OAuth Authentication ==
+#
+# This mode of authentication is the new preferred way
+# of authenticating with Twitter.
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+consumer_key="aMTwTJCOPjPjJQcgIS7VPTJIG"
+consumer_secret="8WQh12NTKbJfmWt2LuaojT8QXGEgBrA4v3fCCwHeXeN75ZXeiO"
+
+# The access tokens can be found on your applications's Details
+# page located at https://dev.twitter.com/apps (located
+# under "Your access token")
+access_token="1034524014478286849-aE0ax20mOESZ4XAxpiURH3e8gXxdTh"
+access_token_secret="tXJL3TlIWi2KCoGfWtD4qwNpz7UeNo7tYiEEyxPLglFC2"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# If the authentication was successful, you should
+# see the name of the account print out
+#print(api.me().name)
+
+# If the application settings are set for "Read and Write" then
+
+# API Core
+
+# CONECTA LA BD
+mydb = mysql.connector.connect(
+	host = "localhost",
+	user = "root",
+	passwd = "root",
+	database = "DolarBOT"
+)
+
+# SELECCIONA EL MAXIMO VALOR DEL DIA
+mycursor.execute("mycursor.execute("select date(date), max(rate) from rates group by date(date) order by date(date) desc limit 1;")
+result = mycursor.fetchall()
+
+items = []
+for x in result:
+	items.append({'date': x[0], 'rate': x[1]})
+
+print('fecha'+items[date][0])
+
+#api.update_status()
